8cdcf691235a9fc0bf3c60ba7b7e5380a54a25f7
yang <247735108@qq.com>
2019-04-02 23:38:50 +0800

    4/2 更改数据库地址

--- src/main/java/com/boot/Main.java
+++ src/main/java/com/boot/Main.java
@@ -1,7 +1,10 @@
 package com.boot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 /** 
 * @author 作者 竺森洋: 
@@ -9,12 +12,15 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 * 类说明 :
 */
 @EnableAutoConfiguration
+@MapperScan(basePackages="com.boot.dao")
+@ComponentScan("com.boot")
 public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(Main.class,args);
+		
 	}
-
+	
 }
  
---
+++ src/main/java/com/boot/controller/PageContrllor.java
@@ -0,0 +1,56 @@
+package com.boot.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.boot.dao.ArticleMapper;
+import com.boot.dao.ArticleTypeMapper;
+import com.boot.entity.Article;
+import com.boot.entity.ArticleType;
+import com.boot.utile.MD5;
+
+/** 
+* @author 作者 竺森洋: 
+* @version 创建时间：2019年3月13日 下午7:25:43 
+* 类说明 :
+*/
+@Controller
+public class PageContrllor {
+
+	
+	@RequestMapping("/index")
+	public String index(){
+		return "index.html";
+	}
+	@RequestMapping("/demo")
+	public String demo(){
+		return "demo.html";
+	}
+	@RequestMapping("/game")
+	public String game(){
+		return "game.html";
+	}
+	@RequestMapping("/life")
+	public String life(){
+		return "life.html";
+	}
+	@RequestMapping("/time")
+	public String time(){
+		return "time.html";
+	}
+	@RequestMapping("/gbook")
+	public String gbook(){
+		return "gbook.html";
+	}
+	@RequestMapping("/info")
+	public String info(){
+		return "info.html";
+	}
+}
+ 
