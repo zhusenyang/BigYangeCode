5db0757c2c180f67b6079e0cbfa391138ffba76b
yang <247735108@qq.com>
2019-04-10 17:31:38 +0800

    二〇一九年四月十日 17:31:33 test shiro

--- src/main/java/com/boot/config/ShiroRealm.java
+++ src/main/java/com/boot/config/ShiroRealm.java
@@ -7,28 +7,42 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class ShiroRealm extends AuthorizingRealm{
-	//一般这里都写的是servic，这里省略直接调用dao
-//    @Autowired
-//    private UUserDao uUserDao;
+import com.boot.dao.UserDao;
+
+public class ShiroRealm extends AuthorizingRealm {
+	// 一般这里都写的是servic，这里省略直接调用dao
+    @Autowired
+    private UserDao userDao;
 //    @Autowired
 //    private URoleDao uRoleDao;
 //    @Autowired
 //    private UPermissionDao uPermissionDao;
-
-    /**
-     * 登录认证
-     * @param authenticationToken
-     * @return
-     * @throws AuthenticationException
-     */
-    @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+    public AuthenticationInfo toLogin(AuthenticationToken authenticationToken) {
+    	doGetAuthenticationInfo(authenticationToken);
+    	return null;
+    }
+    
+	/**
+	 * 登录认证
+	 * 
+	 * @param authenticationToken
+	 * @return
+	 * @throws AuthenticationException
+	 */
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken)
+			throws AuthenticationException {
+		if (authenticationToken==null) {
+			return null;
+		}
+		UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
 //        logger.info("验证当前Subject时获取到token为：" + token.toString());
-        //查出是否有此用户
-        String username = token.getUsername();
+		// 查出是否有此用户
+		String username = token.getUsername();
+		System.out.println("username=\t"+username);
+		
 //        UUser hasUser = uUserDao.selectAllByName(username);
 //
 //        if (hasUser != null) {
@@ -48,17 +62,18 @@ public class ShiroRealm extends AuthorizingRealm{
 //         // 若存在，将此用户存放到登录认证info中，无需自己做密码对比，Shiro会为我们进行密码对比校验
 //            return new SimpleAuthenticationInfo(hasUser, hasUser.getPswd(), getName());
 //        }
+		
+		return null;
+	}
 
-        return null;
-    }
-
-    /**
-     * 权限认证
-     * @param principalCollection
-     * @return
-     */
-    @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+	/**
+	 * 权限认证
+	 * 
+	 * @param principalCollection
+	 * @return
+	 */
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
 //        logger.info("##################执行Shiro权限认证##################");
 //        UUser user = (UUser) principalCollection.getPrimaryPrincipal();
 //        if (user != null) {
@@ -72,6 +87,6 @@ public class ShiroRealm extends AuthorizingRealm{
 //            return info;
 //        }
 //        // 返回null的话，就会导致任何用户访问被拦截的请求时，都会自动跳转到unauthorizedUrl指定的地址
-        return null;
-    }
+		return null;
+	}
 }
